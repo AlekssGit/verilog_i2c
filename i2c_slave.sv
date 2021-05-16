3be973a1e39afac0fdab9b4330835450c7273bc3
Roman <ladyz1roman@gmail.com>
2021-05-16 15:51:46 +0300

    slave worked

--- i2c_slave.sv
+++ i2c_slave.sv
@@ -1,173 +1,171 @@
 module i2c_slave
-#(
-	parameter reg [6:0] ADRS = 7'b1010000
-)
-(
-	input scl,
-	inout sda,
-	input [7:0] txdata,
-	output reg [7:0] rxdata,
-	output reg ack,
-	output reg r,
-	output reg w
-);
+    #(
+        parameter reg [6:0] ADRS=7'b1010000
+    )
+    (
+        input clk,
+        input reset_n,
+        input scl,
+        inout sda,
+        input [7:0] txdata,
+        output reg [7:0] rxdata,
+        output reg ack,
+        output reg r,
+        output reg w
+    );
 
-reg [7:0] txshift;
-reg [7:0] rxshift;
-reg [2:0] counter;
 
-enum int unsigned {
-	IDLE,
-	ADDRESS,
-	READ,
-	WRITE,
-	ACK_A,
-	NACK,
-	ACK_W,
-	ACK_R,
-	RW
-} state, next;
+    logic start_detect = 0;
+    logic stop_detect = 0;
 
-// start detector
-reg start_detect;
-reg start_reset;
+    logic sda_reg = 1;
+    logic scl_reg = 1;
 
-reg _sda;
-assign sda = _sda;
+    logic [7:0] txshift = '1;
+    logic [7:0] rxshift = '1;
+    logic [2:0] counter = '0;
 
-always @(negedge sda, posedge start_reset)
-begin
-	if (start_reset)
-		start_detect = 1'b0;
-	else 
-		start_detect = scl;
-end
+    logic read_strobe = 0;
+    logic write_strobe = 0;
 
-always @(posedge scl)
-begin
-	start_reset = start_detect;
-end
+    logic _sda = 1'bz;
 
-//stop detector
-reg stop_detect;
-reg stop_reset;
+    enum int unsigned {
+        IDLE,
+        ADDRESS,
+        READ,
+        WRITE,
+        ACK_A,
+        NACK,
+        ACK_W,
+        ACK_R,
+        RW
+    } state, next;
 
-always @(posedge sda, posedge stop_reset)
-begin
-	if (stop_reset)
-		stop_detect = 1'b0;
-	else 
-		stop_detect = scl;
-end
+    assign sda = _sda;
 
-always @(negedge sda)
-begin
-	stop_reset = stop_detect;
-end
+    always @(posedge clk)
+        begin
+            start_detect = sda_reg && ~sda && scl;
+            stop_detect = ~sda_reg && sda && scl;
+            read_strobe = ~scl_reg && scl;
+            write_strobe = scl_reg && ~scl;
+            sda_reg = sda;
+            scl_reg = scl;
+        end
 
-//state logic
-always @(negedge scl, posedge stop_detect)
-begin
-	if (stop_detect)
-		state = IDLE;
-	else
-		state = next;
-end
+    always @(posedge clk, negedge reset_n)
+        begin
+            if (~reset_n)
+                begin
+                    state = IDLE;
+                end
+            else
+                begin
+                    state = next;
+                end
+        end
 
-always_comb
-begin
-	case (state)
-		IDLE: next = start_detect ? ADDRESS : IDLE;
-		ADDRESS: next = counter == 6 ? RW : ADDRESS;
-		RW: next = ADRS == rxshift[6:0] ? ACK_A : NACK;
-		ACK_A: next = rxshift[0] ? WRITE : READ;
-		NACK: next = IDLE;
-		READ: next = counter == 7 ? ACK_R : start_detect ? ADDRESS : READ;
-		WRITE: next = counter == 7 ? ACK_W : start_detect ? ADDRESS : WRITE;
-		ACK_W: next = sda ? IDLE : WRITE;
-		ACK_R: next = READ;
-	endcase
-end
+    always_comb
+        begin
+            case (state)
+                IDLE: next = start_detect ? ADDRESS:IDLE;
+                ADDRESS: next = counter == 7 ? RW:ADDRESS;
+                RW: next = read_strobe ? (ADRS == rxshift[7:1] ? ACK_A:NACK):RW;
+                ACK_A: next = read_strobe ? (rxshift[0] ? WRITE:READ):ACK_A;
+                NACK: next = read_strobe ? IDLE:NACK;
+                READ: next = read_strobe && counter == 0 ? ACK_R:start_detect ? ADDRESS:READ;
+                WRITE: next = read_strobe && counter == 0 ? ACK_W:start_detect ? ADDRESS:WRITE;
+                ACK_W: next = read_strobe ? (sda ? IDLE:WRITE):ACK_W;
+                ACK_R: next = read_strobe ? READ:ACK_R;
+            endcase
+        end
 
-always @(negedge scl, posedge stop_detect)
-begin
-	if (stop_detect)
-		ack = 1'b0;
-	else
-	begin
-		if (state == ACK_W || state == ACK_R || state == ACK_A && r)
-			ack = ~sda;
-		else
-			ack = 1'b0;
-	end
-end
+    always @(posedge clk, posedge stop_detect)
+        begin
+            if (stop_detect)
+                ack = 1'b0;
+            else if (read_strobe)
+                begin
+                    if (state == ACK_W || state == ACK_R || state == ACK_A && r)
+                        ack = ~sda;
+                    else
+                        ack = 1'b0;
+                end
+        end
 
-always @(negedge scl, posedge stop_detect)
-begin
-	if (stop_detect)
-	begin
-		r = 1'b0;
-		w = 1'b0;
-	end
-	else
-	begin
-		if (state == IDLE || state == ADDRESS)
-		begin
-			r = 1'b0;
-			w = 1'b0;
-		end
-		else if (state == RW && ADRS == rxshift[6:0])
-		begin
-			r = sda;
-			w = ~sda;
-		end
-	end
-end
+    always @(posedge clk, posedge stop_detect)
+        begin
+            if (stop_detect)
+                begin
+                    r = 1'b0;
+                    w = 1'b0;
+                end
+            else if (read_strobe)
+                begin
+                    if (state == IDLE || state == ADDRESS)
+                        begin
+                            r = 1'b0;
+                            w = 1'b0;
+                        end
+                    else if (state == RW)
+                        begin
+                            r = sda;
+                            w = ~sda;
+                        end
+                end
+        end
 
-always_comb
-begin
-	case (state)
-		IDLE, ADDRESS, RW, NACK, READ, ACK_W:
-		begin
-			_sda = 1'bz;
-		end
-		ACK_A, ACK_R:
-		begin
-			_sda = 1'b0;
-		end
-		WRITE:
-		begin
-			_sda = txshift[7] ? 1'bz : 1'b0;
-		end
-	endcase
-end
+    always @(posedge clk)
+        begin
+            if (write_strobe)
+                begin
+                    case (state)
+                        IDLE, ADDRESS, RW, NACK, READ, ACK_W:
+                            begin
+                                _sda = 1'bz;
+                            end
+                        ACK_A, ACK_R:
+                            begin
+                                _sda = 1'b0;
+                            end
+                        WRITE:
+                            begin
 
-always @(negedge scl, posedge start_detect)
-begin
-	if (start_detect)
-		counter = '0;
-	else if (state == ADDRESS || state == READ || state == WRITE || state == RW)
-		counter++;
-end
+                                _sda = txshift[7] ? 1'bz:1'b0;
+                            end
+                    endcase
+
+                end
+        end
+
+    always @(posedge clk)
+        begin
+            if (state == IDLE)
+                counter = '0;
+            else if (read_strobe && (state == ADDRESS || state == READ ||  state == RW) || write_strobe && state == WRITE)
+                counter++;
+        end
 
 //read sda
-always @(negedge scl)
-begin
-	if (state == ADDRESS || state == READ || state == RW)
-		rxshift = {rxshift[6:0], sda};
-end
+    always @(posedge clk)
+        begin
+            if (read_strobe && (state == ADDRESS || state == READ || state == RW))
+                rxshift = {rxshift[6:0], sda};
+        end
 
-always @(posedge scl)
-begin
-	if (state == ACK_R)
-		rxdata = rxshift;
-end
+    always @(posedge clk)
+        begin
+            if (read_strobe && state == ACK_R)
+                rxdata = rxshift;
+        end
 
 //write to sda
-always @(negedge scl)
-begin
-	if (state == WRITE || state == ACK_A && r || state == ACK_W)
-		txshift = (state == WRITE) ? {txshift[6:0], 1'b1} : txdata;
-end
+    always @(posedge clk)
+        begin
+            if (write_strobe && (state == WRITE || state == ACK_A && r || state == ACK_W))
+                txshift = (state == WRITE) ? {txshift[6:0], 1'b1}:txdata;
+        end
 
-endmodule 
+
+endmodule : i2c_slave
---
+++ i2c_slave_tb.sv
@@ -0,0 +1,234 @@
+module i2c_slave_tb();
+
+
+    logic clock;
+    wire scl;
+    wire sda;
+    logic [7:0] s_rxdata;
+    logic [7:0] s_txdata;
+
+    logic [6:0] address;
+    logic [7:0] rxdata;
+    logic [7:0] txdata;
+
+    pullup (weak1) pull_sda(sda);
+    pullup (weak1) pull_scl(scl);
+
+    logic _scl;
+    logic _sda;
+
+    assign sda = _sda;
+    assign scl = _scl;
+    int i;
+
+    i2c_slave#(.ADRS(7'h50)) slave(
+        .scl(scl),
+        .sda(sda),
+        .clk(clock),
+        .reset_n(1'b1),
+        .rxdata(s_rxdata),
+        .txdata(s_txdata)
+    );
+
+    initial
+        begin
+            _sda = 1'bz;
+            _scl = 1'bz;
+            clock = 1'b0;
+
+            // slave writes data
+
+            s_txdata = 8'hcd;
+            address = 7'h50;
+
+            //start
+            #20;
+            _sda = 1'b0;
+            #20;
+            _scl = 1'b0;
+
+            // address
+            for (i = 0; i < 7; i++)
+                begin
+                    #20;
+                    _sda = address[6-i] ? 1'bz:1'b0;
+                    #20;
+                    _scl = 1'bz;
+                    #40;
+                    _scl = 1'b0;
+                end
+
+            // rw
+            #20;
+            _sda = 1'bz; // read
+            #20;
+            _scl = 1'bz;
+            #40;
+            _scl = 1'b0;
+
+            // wait ack
+            #20;
+            _sda = 1'bz;
+            #20;
+            _scl = 1'bz;
+            #40;
+            _scl = 1'b0;
+
+            // read
+            for (i = 0; i < 8; i++)
+                begin
+                    #40;
+                    _scl = 1'bz;
+                    rxdata[7-i] = sda;
+                    #40;
+                    _scl = 1'b0;
+                end
+            // check if rxdata = s_txdata
+
+            // nack
+            #20;
+            _sda = 1'bz;
+            #20;
+            _scl = 1'bz;
+            #40;
+            _scl = 1'b0;
+
+            // stop
+            #20;
+            _sda = 1'b0;
+            #20;
+            _scl = 1'bz;
+            #20;
+            _sda = 1'bz;
+
+            // end slave writes data
+
+            #100;
+
+            // wrong slave address
+
+            address = 7'h51;
+
+            //start
+            #20;
+            _sda = 1'b0;
+            #20;
+            _scl = 1'b0;
+
+            // address
+            for (i = 0; i < 7; i++)
+                begin
+                    #20;
+                    _sda = address[6-i] ? 1'bz:1'b0;
+                    #20;
+                    _scl = 1'bz;
+                    #40;
+                    _scl = 1'b0;
+                end
+
+            // rw
+            #20;
+            _sda = 1'bz; // read
+            #20;
+            _scl = 1'bz;
+            #40;
+            _scl = 1'b0;
+
+            // wait ack
+            #20;
+            _sda = 1'bz;
+            #20;
+            _scl = 1'bz;
+            // check whether sda is 1 or not
+            #40;
+            _scl = 1'b0;
+
+            // stop
+            #20;
+            _sda = 1'b0;
+            #20;
+            _scl = 1'bz;
+            #20;
+            _sda = 1'bz;
+
+            // end wrong slave address
+
+            #100;
+
+            // slave read data
+
+            txdata = 8'had;
+            address = 7'h50;
+
+            //start
+            #20;
+            _sda = 1'b0;
+            #20;
+            _scl = 1'b0;
+
+            // address
+            for (i = 0; i < 7; i++)
+                begin
+                    #20;
+                    _sda = address[6-i] ? 1'bz:1'b0;
+                    #20;
+                    _scl = 1'bz;
+                    #40;
+                    _scl = 1'b0;
+                end
+
+            // rw
+            #20;
+            _sda = 1'b0; // write
+            #20;
+            _scl = 1'bz;
+            #40;
+            _scl = 1'b0;
+
+            // wait ack
+            #20;
+            _sda = 1'bz;
+            #20;
+            _scl = 1'bz;
+            #40;
+            _scl = 1'b0;
+
+            // write
+            for (i = 0; i < 8; i++)
+                begin
+                    #20;
+                    _sda = txdata[7-i] ? 1'bz:1'b0;
+                    #20;
+                    _scl = 1'bz;
+                    #40;
+                    _scl = 1'b0;
+                end
+            // check if txdata = s_rxdata
+
+            // nack
+            #20;
+            _sda = 1'bz;
+            #20;
+            _scl = 1'bz;
+            #40;
+            _scl = 1'b0;
+
+            // stop
+            #20;
+            _sda = 1'b0;
+            #20;
+            _scl = 1'bz;
+            #20;
+            _sda = 1'bz;
+
+            // end slave read data
+
+        end
+
+    always #5
+        begin
+            clock = ~clock;
+        end
+
+
+endmodule: i2c_slave_tb
