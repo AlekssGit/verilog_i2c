4dff88e611163f2749949a53a7743b44408845e1
AlekssGit <alvasin98@gmail.com>
2021-05-18 17:26:19 +0300

    worked with tb.sv

--- tb.sv
+++ tb.sv
@@ -37,13 +37,13 @@ pullup (weak1) pull_scl (scl);
 logic m_clock;
 
 //end variables for slave
-imp_generator gen (.address(address), .clock(clock), .m_reset(m_reset), .m_enable(m_enable), .m_rw(m_rw), .m_tx_data(m_tx_data), .m_restart(m_restart), .s_50_tx_data(s_50_tx_data), .s_51_tx_data(s_51_tx_data), .s_50_reset(s_50_reset), .s_51_reset(s_51_reset), .m_clock(m_clock));
+imp_generator gen (.address(address), .clock(clock), .m_reset(m_reset), .m_enable(m_enable), .m_rw(m_rw), .m_tx_data(m_tx_data), .m_restart(m_restart), .s_50_tx_data(s_50_tx_data), .s_51_tx_data(s_51_tx_data), .s_50_reset(s_50_reset), .s_51_reset(s_51_reset), .m_clock(m_clock), .m_ack(m_ack), .m_rx_data(m_rx_data), .s_50_rx_data(s_50_rx_data), .s_51_rx_data(s_51_rx_data), .m_nack(m_nack));
 i2c_master master ( .clk(m_clock), .txdata(m_tx_data), .address(address), .enable(m_enable), .rw(m_rw), .restart(m_restart), .reset_n(m_reset), .scl(scl), .sda(sda), .rxdata(m_rx_data), .ack(m_ack), .nack(m_nack), .ready(m_ready));
 i2c_slave #(7'h50) slave_50 (.clk(clock), .reset_n(s_50_reset), .scl(scl), .sda(sda) , .txdata(s_50_tx_data), .rxdata(s_50_rx_data), .ack(s_50_ack), .r(s_50_r), .w(s_50_w));
 i2c_slave #(7'h51) slave_51 (.clk(clock), .reset_n(s_51_reset), .scl(scl), .sda(sda) , .txdata(s_51_tx_data), .rxdata(s_51_rx_data), .ack(s_51_ack), .r(s_51_r), .w(s_51_w));
 endmodule
 
-module imp_generator(address, clock, m_reset, m_enable, m_rw, m_tx_data, m_restart, s_50_tx_data, s_51_tx_data, s_50_reset, s_51_reset, m_clock);
+module imp_generator(address, clock, m_reset, m_enable, m_rw, m_tx_data, m_restart, s_50_tx_data, s_51_tx_data, s_50_reset, s_51_reset, m_clock, m_ack, m_rx_data, s_50_rx_data, s_51_rx_data, m_nack);
 output logic [6:0] address;
 output logic clock;
 output logic m_reset;
@@ -56,10 +56,24 @@ output logic [7:0] s_51_tx_data;
 output logic s_50_reset;
 output logic s_51_reset;
 output logic m_clock;
-
+input logic m_ack;
+input logic  [7:0] m_rx_data;
+input logic  [7:0] s_50_rx_data;
+input logic  [7:0] s_51_rx_data;
+input logic m_nack;
 int i;
 int main_counter;
 int master_restart_counter;
+logic ack_posedge;
+
+enum int unsigned {
+	WRITE,
+	READ,
+	ACK_1_W,
+	ACK_1_R,
+	ACK_2,
+	RESTART
+} state;
 
 initial
 begin
@@ -79,6 +93,9 @@ begin
 	main_counter = 0;
 	master_restart_counter = 0;
 
+	state = WRITE;
+	ack_posedge = 1'b0;
+
         #5;
         m_reset = 1;
         #5 
@@ -89,6 +106,99 @@ begin
 	m_enable =  1;
 	forever
 	begin
+		case(state)
+		WRITE: 
+		begin
+
+			//m_rx_data = 8'h00;
+			//m_restart = 1'b0;
+			m_enable = 1'b1;
+			m_rw = 1'b0;
+			address = 7'h50;
+		end
+		READ:
+		begin
+			
+			//s_50_rx_data = 8'h00;
+			//s_51_rx_data = 8'h00;
+			//m_restart = 1'b0;
+			m_enable = 1'b1;
+			m_rw = 1'b1;
+			address = 7'h51;
+		end
+		ACK_1_R: 
+		begin
+			//m_rw = ~m_rw;
+			//m_restart = 1'b1;
+			m_enable = 1'b0;
+			if(m_nack)
+			begin
+				state = ACK_2;
+			end
+		end
+		ACK_1_W: 
+		begin
+			//m_rw = ~m_rw;
+			//m_restart = 1'b1;
+			m_enable = 1'b0;
+		end
+		ACK_2:
+		begin
+			m_restart = 1'b0;
+			m_enable = 1'b1;
+			
+			state = (m_rw) ? READ : WRITE;
+			if(state == WRITE)
+			begin
+				s_50_tx_data++;
+				s_51_tx_data++;
+			end
+			else if(state == READ)
+			begin
+				m_tx_data++;
+			end
+		end
+		RESTART:
+		begin
+		end
+		endcase
+
+		if(m_nack == 1'b1)
+		begin
+			ack_posedge = 0;
+		end
+
+		if(m_ack == 1'b1)
+		begin
+			ack_posedge = 1'b1;
+		end	
+		else
+		begin
+			if(ack_posedge)
+			begin
+				ack_posedge = 0;
+				if(state == WRITE)
+				begin					
+					state = ACK_1_W;
+					m_rw = ~m_rw;
+				end
+				else if (state == READ)
+				begin
+					state = ACK_1_R;
+					m_rw = ~m_rw;
+				end
+				else if(state == ACK_1_W)
+					state = ACK_2;
+			end
+		end
+
+		m_clock = (i % 2) ? ~m_clock : m_clock;
+		#5 
+		clock=1;
+          	#5 
+		clock=0;
+		i++;
+/*
 		for(i = 0; i < 140; i++)
 		begin
 			case(i)
@@ -106,6 +216,7 @@ begin
 		else
 			address = address - 1'b1;
 		m_rw = ~m_rw;
+*/
 	end
 end
 
