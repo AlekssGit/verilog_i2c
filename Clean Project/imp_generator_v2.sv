8a87ea0e72009b346cb5633eef7c2d6250272292
AlekssGit <alvasin98@gmail.com>
2021-06-02 21:42:06 +0300

    added testbench randomization

--- Clean Project/imp_generator_v2.sv
+++ Clean Project/imp_generator_v2.sv
@@ -1,4 +1,16 @@
 module imp_generator_v2(address, clock, m_reset, m_enable, m_rw, m_tx_data, m_restart, s_50_tx_data, s_51_tx_data, s_50_reset, s_51_reset, m_clock, m_ack, m_rx_data, s_50_rx_data, s_51_rx_data, m_ready);
+
+class TEST_DATA;
+	randc bit [6 : 0] address;
+	constraint address_valid { address inside { [7'h4C : 7'h52] }; }
+	rand bit [7 : 0] master_data;
+	rand bit [7 : 0] slave_1_data;
+	rand bit [7 : 0] slave_2_data;
+	constraint data_valid { master_data > 0; slave_1_data > 0; slave_2_data > 0; }
+	rand bit master_reset;
+endclass;
+TEST_DATA rand_data;
+
 output logic [6:0] address;
 output logic clock;
 output logic m_reset;
@@ -20,6 +32,8 @@ int i;
 logic ack_posedge;
 
 int cnt_idle;
+int test_cnt;
+int cnt_reset;
 
 logic [7:0] check_data;
 
@@ -34,16 +48,19 @@ enum int unsigned {
 
 initial
 begin
+	rand_data = new();
+	assert(rand_data.randomize());
+
 	clock = 0; 
 	m_clock = 1'b0;
 	m_reset = 1; 
 	m_rw = 1'b0;
 	m_restart = 0;
 	m_enable = 0;
-	m_tx_data = 8'hFE; 
-	address = 7'h50;
-	s_50_tx_data = 8'hCC;
-	s_51_tx_data = 8'hBB;
+	m_tx_data    = rand_data.master_data; 	//8'hFE; 
+	address      = rand_data.address; 	//7'h50;
+	s_50_tx_data = rand_data.slave_1_data; 	//8'hCC;
+	s_51_tx_data = rand_data.slave_1_data; 	//8'hBB;
 	s_50_reset = 1'b1;
 	s_51_reset = 1'b1;
 
@@ -52,6 +69,8 @@ begin
 	state = WRITE;
 	ack_posedge = 1'b0;
 	cnt_idle = 0;
+	test_cnt = 0;
+	cnt_reset = 0;
 
         #5;
         m_reset = 1;
@@ -75,20 +94,34 @@ begin
 					if(state == WRITE)
 					begin					
 						state = ACK_1_W;
+						cnt_reset = 0;
 					end
 					else if (state == READ)
 					begin
 						state = ACK_1_R;
 					end
 				end
+				else  if(m_ready & (test_cnt > 10))
+				begin
+					test_cnt = 0;
+					m_enable = 1'b0;
+					state = IDLE;
+				end
+				else
+				begin
+					test_cnt++;
+				end
 			end
+
 		end
 		ACK_1_R: 
 		begin
 			m_enable = 1'b0;
 			if(m_ready)
 			begin
+				//assert(rand_data.randomize());
 				state = IDLE;
+				test_cnt = 0;
 				cnt_idle = 0;
 				ack_posedge = 0;
 				if(check_data == m_rx_data)
@@ -100,15 +133,27 @@ begin
 		ACK_1_W: 
 		begin
 			m_enable = 1'b0;
+			if(cnt_reset == 4)
+			begin
+				cnt_reset = 0;
+				m_reset = rand_data.master_reset;
+			end
+			else 
+				cnt_reset++;
+
 			if(m_ready)
 			begin
 				state = IDLE;
+				test_cnt = 0;
 				cnt_idle = 0;
 				ack_posedge = 0;
-				if(check_data == ((address == 7'h50) ? s_50_rx_data : s_51_rx_data))
-					$info("success data write from master to slave");
-				else
-					$warning("wrong data write from master to slave");
+				if((address == 7'h50) || (address == 7'h51))
+				begin
+					if(check_data == ((address == 7'h50) ? s_50_rx_data : s_51_rx_data))
+						$info("success data write from master to slave");
+					else
+						$warning("wrong data write from master to slave");
+				end
 			end
 		end
 
@@ -120,19 +165,21 @@ begin
 			end
 			else
 			begin
+				m_reset = 1'b1;
+				assert(rand_data.randomize());
 				cnt_idle = 0;
 				m_rw = ~m_rw;
 				state = (m_rw) ? READ : WRITE;
-				address = (state == WRITE) ? 7'h50 :7'h51;
+				address = rand_data.address; //(state == WRITE) ? 7'h50 :7'h51;
 				if(state == WRITE)
 				begin
-					s_50_tx_data++;
-					s_51_tx_data++;
+					s_50_tx_data = rand_data.slave_1_data; //s_50_tx_data++;
+					s_50_tx_data = rand_data.slave_1_data; //s_51_tx_data++;
 					check_data = m_tx_data;
 				end
 				else if(state == READ)
 				begin
-					m_tx_data++;
+					m_tx_data = rand_data.master_data;//m_tx_data++;
 					check_data = (address == 7'h50) ? s_50_tx_data : s_51_tx_data;
 				end
 			end
